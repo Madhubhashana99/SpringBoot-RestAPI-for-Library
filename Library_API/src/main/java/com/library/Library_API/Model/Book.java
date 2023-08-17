0d130fa3032e6a598e916ac665ece2cce6e53df8
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-17 19:54:54 +0530

    Book Model

---
+++ Library_API/src/main/java/com/library/Library_API/Model/Book.java
@@ -0,0 +1,20 @@
+package com.library.Library_API.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+
+
+}
